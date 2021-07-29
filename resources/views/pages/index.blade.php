696adb354a18422e6601145e03bec5fb31a605ef
Clovis <dabaclovis@yahoo.com>
2021-07-29 13:22:07 -0400

    fast_routing_in_laravel

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+      <div class="card-body">
+        <blockquote class="blockquote">
+          <p class="" style="font-size: 22px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, dolorem. Voluptatum quaerat soluta hic et, libero cupiditate aut nulla fugiat deserunt maiores amet ipsam recusandae sapiente dolore molestiae natus? Odit similique ullam fugiat autem. Voluptatum odit quod pariatur officiis temporibus harum maiores quas nihil officia praesentium soluta deserunt at magnam voluptas, eveniet totam eius! Quos, animi amet ipsa consequatur minus deserunt quo perferendis vel tempore distinctio accusamus hic.</p>
+          <footer class="card-blockquote"><cite title="Source title"></cite>&nbsp;
+        <?php echo 'Today is '.'.' .date('F / d  / Y')?>
+        </footer>
+        </blockquote>
+      </div>
+    </div>
+    <div class="card-body">
+        <h4>Detail</h4>
+        <p style="font-size: 22px;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia, debitis! Sint delectus
+             ex quibusdam ab, voluptate sed in eligendi magni! Commodi, consequatur laboriosam. Optio laudantium nobis eius 
+             porro excepturi nulla eum tempora, magnam sed necessitatibus molestiae similique, vel tempore voluptates hic 
+             itaque praesentium voluptate dolorem fugiat, vero reprehenderit suscipit distinctio id beatae! Voluptas perferendis
+              illo aspernatur, fuga dolores est nulla praesentium natus. Modi sit sint eligendi quis perspiciatis odio excepturi
+               optio doloribus enim veniam quam atque accusamus voluptate aperiam quos, illum quidem fugit! Nihil, consectetur 
+               error eos, et magnam aliquid explicabo ex, dolores itaque fuga similique delectus. Eligendi, quod! Aut ducimus 
+               fuga deserunt, labore molestiae quaerat dignissimos, ipsam quo repellendus libero illum ab adipisci consectetur
+                porro dolorem, officiis ratione iste eius suscipit similique exercitationem aspernatur numquam! Dicta voluptate, 
+                sapiente blanditiis temporibus officia ad culpa, suscipit facere rerum, praesentium repellendus harum. Cupiditate
+                 sequi voluptatibus numquam molestias atque dolorum eum explicabo magnam impedit nam, libero saepe suscipit maxime,
+                  aperiam neque voluptate optio itaque, nobis nisi voluptatum quo reiciendis est? A vero atque eius! Perspiciatis 
+                  ipsa eligendi voluptatibus eaque natus dolore culpa nostrum. Earum illo rem modi 
+            alias eveniet, provident error quae quam quaerat, nulla iure quia ad expedita. Officia quae nemo minima?</p>
+    </div>
+@endsection
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2> Our Services Page</h2>
+    <div class="card">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">SEO</li>
+            <li class="list-group-item">Web learning</li>
+            <li class="list-group-item">Web Design</li>
+        </ul>
+    </div>
+    <div class="card-body">
+        <p style="font-size: 22px;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia, debitis! Sint delectus
+             ex quibusdam ab, voluptate sed in eligendi magni! Commodi, consequatur laboriosam. Optio laudantium nobis eius 
+             porro excepturi nulla eum tempora, magnam sed necessitatibus molestiae similique, vel tempore voluptates hic 
+             itaque praesentium voluptate dolorem fugiat, vero reprehenderit suscipit distinctio id beatae! Voluptas perferendis
+              illo aspernatur, fuga dolores est nulla praesentium natus. Modi sit sint eligendi quis perspiciatis odio excepturi
+               optio doloribus enim veniam quam atque accusamus voluptate aperiam quos, illum quidem fugit! Nihil, consectetur 
+               error eos, et magnam aliquid explicabo ex, dolores itaque fuga similique delectus. Eligendi, quod! Aut ducimus 
+               fuga deserunt, labore molestiae quaerat dignissimos, ipsam quo repellendus libero illum ab adipisci consectetur
+                porro dolorem, officiis ratione iste eius suscipit similique exercitationem aspernatur numquam! Dicta voluptate, 
+                sapiente blanditiis temporibus officia ad culpa, suscipit facere rerum, praesentium repellendus harum. Cupiditate
+                 sequi voluptatibus numquam molestias atque dolorum eum explicabo magnam impedit nam, libero saepe suscipit maxime,
+                  aperiam neque voluptate optio itaque, nobis nisi voluptatum quo reiciendis est? A vero atque eius! Perspiciatis 
+                  ipsa eligendi voluptatibus eaque natus dolore culpa nostrum. Earum illo rem modi 
+            alias eveniet, provident error quae quam quaerat, nulla iure quia ad expedita. Officia quae nemo minima?</p>
+    </div>
+@endsection
